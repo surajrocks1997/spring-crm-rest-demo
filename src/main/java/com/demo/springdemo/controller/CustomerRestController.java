9c72b29b9eebda035f8d18a0ab02a4deddd97bd3
Suraj Gupta <surajrocks1997@gmail.com>
2022-08-18 13:10:29 +0530

    get list of customer and customer by id

---
+++ src/main/java/com/demo/springdemo/controller/CustomerRestController.java
@@ -0,0 +1,30 @@
+package com.demo.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.springdemo.entity.Customer;
+import com.demo.springdemo.service.CustomerService;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerRestController {
+
+	@Autowired
+	private CustomerService customerService;
+
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return customerService.getCustomers();
+	}
+
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		return customerService.getCustomer(customerId);
+	}
+}
